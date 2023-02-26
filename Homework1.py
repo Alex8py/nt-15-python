20911d5e5736b067222f4c0b2cb4498fb77878c2
Ursaru I. Alexandru <ioanalaexandruursaru@gmail.com>
2023-02-26 21:00:29 +0200

    My #1 awesome feature

---
+++ Homework1.py
@@ -0,0 +1,16 @@
+my_numbers = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+my_numbers.sort()
+print(my_numbers)
+
+my_sliced_numbers=my_numbers[1::2]
+print(my_sliced_numbers)
+
+my_sliced_numbers=my_numbers[::2]
+print(my_sliced_numbers)
+
+my_sliced_numbers=my_numbers[2:9:3]
+print(my_sliced_numbers)
+
+my_numbers.reverse()
+print(my_numbers)
+
